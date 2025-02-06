b8c1722a61948422c1b99a52b6743225f2062027
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-02-05 23:26:59 -0300

    Created the map C file to separate the functions.

---
+++ map.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "map.h"
+
+void walkOnTheMap(MAP* m, int originX, int originY, int newX, int newY) {
+	char hero = m->matriz[originX][originY];
+	m->matriz[newX][newY] = hero;
+	m->matriz[originX][originY] = EMPTY;
+}
+
+int isEmpty(MAP* m, int x, int y) {
+	return m->matriz[x][y] == EMPTY;
+}
+
+int isValid(MAP* m, int x, int y) {
+	if (x >= m->lines) return 0;
+	if (y >= m->columns) return 0;
+	return 1;
+}
+
+void findMap(MAP* m, POSITION* p, char c) {
+	for (int i = 0; i < m->lines; i++) {
+		for (int j = 0; j < m->columns; j++) {
+			if (m->matriz[i][j] == c) {
+				p->x = i;
+				p->y = j; 
+				break;
+			}
+		}
+	}
+}
+
+void releaseMap(MAP* m) {
+	for (int i = 0; i < m->lines; i++) {
+		free(m->matriz[i]);
+	}
+	free(m->matriz);
+}
+
+void readMap(MAP* m) {
+	FILE* file;
+	file = fopen("map.txt", "r");
+	if (file == 0) {
+		printf("Erro na leitura do mapa.\n");
+		exit(1);
+	}
+
+	fscanf(file, "%d %d", &m->lines, &m->columns);
+
+	allocMap(m);
+
+	for (int i = 0; i <= 4; i++) {
+		fscanf(file, "%s", m->matriz[i]);
+	}
+
+	fclose(file);
+}
+
+void allocMap(MAP* m) {
+	m->matriz = malloc(sizeof(char*) * m->lines);
+
+	for (int i = 0; i < m->lines; i++) {
+		m->matriz[i] = malloc(sizeof(char) * (m->columns + 1));
+	}
+}
+
+void drawMap(MAP* m) {
+	for (int i = 0; i <= 4; i++) {
+		printf("%s\n", m->matriz[i]);
+	}
+}
---
+++ map.h
@@ -0,0 +1,28 @@
+#define HERO '@'
+#define EMPTY '.'
+#define VERTICAL_WALL '|'
+#define HORIZONTAL_WALL '_'
+
+struct map {
+	char** matriz;
+	int lines;
+	int columns;
+};
+
+typedef struct map MAP;
+
+struct position {
+	int x;
+	int y;
+};
+
+typedef struct position POSITION;
+
+void releaseMap(MAP* m);
+void readMap(MAP* m);
+void allocMap(MAP* m);
+void drawMap(MAP* m);
+void findMap(MAP* m, POSITION* p, char c);
+int isEmpty(MAP* m, int x, int y);
+int isValid(MAP* m, int x, int y);
+void walkOnTheMap(MAP* m, int originX, int originY, int newX, int newY);
