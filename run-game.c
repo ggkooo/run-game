624f15386d45b36dc533aacd4c087dc60556f64d
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-02-07 20:45:41 -0300

    Final game update

--- run-game.c
+++ run-game.c
@@ -3,9 +3,11 @@
 #include <time.h>
 #include "run-game.h"
 #include "map.h"
+#include "ui.h"
 
 MAP m;
 POSITION hero;
+int hasPill = 0; 
 
 int whereTheGhostGo(int originX, int originY, int* newX, int* newY) {
 	int option[4][2] = {
@@ -19,7 +21,7 @@ int whereTheGhostGo(int originX, int originY, int* newX, int* newY) {
 	for (int i = 0; i < 10; i++) {
 		int newPosition = rand() % 4;
 
-		if (isValid(&m, option[newPosition][0], option[newPosition][1]) && isEmpty(&m, option[newPosition][0], option[newPosition][1])) {
+		if (canWalk(&m, GHOST, option[newPosition][0], option[newPosition][1])) {
 			*newX = option[newPosition][0];
 			*newY = option[newPosition][1];
 			return 1;
@@ -52,7 +54,9 @@ void ghosts() {
 }
 
 int finish() {
-	return 0;
+	POSITION position;
+	int find = findMap(&m, &position, HERO);
+	return !find;
 }
 
 int isDirection(char direction) {
@@ -80,24 +84,57 @@ void move(char direction) {
 			break;
 	}
 
-	if (!isValid(&m, nextX, nextY)) return;
-	if (!isEmpty(&m, nextX, nextY)) return;
+	if (!canWalk(&m, HERO, nextX, nextY)) return;
+
+	if (isHero(&m, PILL, nextX, nextY)) {
+		hasPill = 1;
+	}
 
 	walkOnTheMap(&m, hero.x, hero.y, nextX, nextY);
 	hero.x = nextX;
 	hero.y = nextY;
- }
+}
+
+void pillExplode() {
+	if(!hasPill) return;
+
+	pillExplode2(hero.x, hero.y, 0, 1, 3);
+	pillExplode2(hero.x, hero.y, 0, -1, 3);
+	pillExplode2(hero.x, hero.y, 1, 0, 3);
+	pillExplode2(hero.x, hero.y, -1, 0, 3);
+
+	hasPill	= 0;
+}
+
+void pillExplode2(int x, int y, int sumX, int sumY, int quantity) {
+	if (quantity == 0) return;
+
+	int newX = x + sumX;
+	int newY = y + sumY;
+
+	if (!isValid(&m, newX, newY)) return;
+	if (isWall(&m, newX, newY)) return;
+
+	m.matriz[newX][newY] = EMPTY;
+	pillExplode2(newX, newY, sumX, sumY, (quantity - 1));
+}
 
 int main() {
 	readMap(&m);
 	findMap(&m, &hero, HERO);
 
 	do {
+		printf("Tem pilula: %s\n", (hasPill ? "Sim" : "Nao"));
 		drawMap(&m);
+
 		char command;
 		scanf(" %c", &command);
 		move(command);
+
+		if (command == BOMB) pillExplode();
+
 		ghosts();
+		system("cls");
 	} while(!finish());
 
 	releaseMap(&m);
--- run-game.h
+++ run-game.h
@@ -1,9 +1,17 @@
+#ifndef _RUN_H_
+#define _RUN_H_
+
 #define TOP 'w'
 #define BOTTOM 's'
 #define RIGHT 'd'
 #define LEFT 'a'
+#define BOMB 'b'
 
 int finish();
 void move(char direction);
 int isDirection(char direction);
 void fanstasms();
+void pillExplode();
+void pillExplode2(int x, int y, int sumX, int sumY, int quantity);
+
+#endif
